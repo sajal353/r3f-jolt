31b91172ff1644a875c759045e4fa773631b6b29
sajal353 <sajalbiswas353@gmail.com>
2024-02-15 04:35:11 +0600

    Update error message in useJolt hook

--- src/Jolt/useJolt.ts
+++ src/Jolt/useJolt.ts
@@ -5,7 +5,7 @@ export const useJolt = () => {
   const api = useContext(joltContext);
 
   if (api === null) {
-    throw new Error("useJolt must be used within a Jolt Physics Provider");
+    throw new Error("Jolt hooks must be used within a Jolt Physics Provider");
   }
 
   const { Jolt, joltInterface, physicsSystem, bodyInterface, layers } = api;
